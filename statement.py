ca0b03f2372671ef696f6995dddc2e237205f7b3
Fran Iglesias <franiglesias@mac.com>
2022-09-29 12:32:46 +0200

    Remove uses of primitive total_amount

--- statement.py
+++ statement.py
@@ -4,7 +4,6 @@ from domain.amount import Amount
 
 
 def statement(invoice, plays):
-    total_amount = 0
     invoice_amount = Amount(0)
     volume_credits = 0
     result = f'Statement for {invoice["customer"]}\n'
@@ -63,10 +62,9 @@ def statement(invoice, plays):
         line = f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
 
         result += line
-        total_amount += this_amount
         invoice_amount = invoice_amount.add(Amount(this_amount))
         volume_credits += performance_credits
 
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
+    result += f'Amount owed is {format_as_dollars(invoice_amount.current()//100)}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
