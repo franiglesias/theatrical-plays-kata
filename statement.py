d9d655ed9d7814fcf96b7c2bc6740e40a5f3a9eb
Fran Iglesias <franiglesias@mac.com>
2022-09-30 11:26:41 +0200

    Using Performance instead of inner functions

--- statement.py
+++ statement.py
@@ -15,34 +15,13 @@ def statement(invoice, plays):
     def format_as_dollars(amount):
         return f"${amount:0,.2f}"
 
-    def calculate_performance_amount(perf, play, performance):
+    def calculate_performance_amount(play, performance):
         if play['type'] == "tragedy":
-            return calculate_amount_for_tragedy(perf)
+            return performance.calculate_amount_for_tragedy()
         if play['type'] == "comedy":
-            return calculate_amount_for_comedy(perf)
+            return performance.calculate_amount_for_comedy()
 
         raise ValueError(f'unknown type: {play["type"]}')
-
-    def calculate_amount_for_comedy(perf):
-        return Amount(30000) \
-            .add(extra_amount_for_high_audience_in_comedy(perf)) \
-            .add(Amount(300 * perf['audience']))
-
-    def extra_amount_for_high_audience_in_comedy(perf):
-        if perf['audience'] <= 20:
-            return Amount(0)
-
-        return Amount(10000 + 500 * (perf['audience'] - 20))
-
-    def calculate_amount_for_tragedy(perf):
-        return Amount(40000) \
-            .add(extra_amount_for_high_audience_in_tragedy(perf))
-
-    def extra_amount_for_high_audience_in_tragedy(perf):
-        if perf['audience'] <= 30:
-            return Amount(0)
-
-        return Amount(1000 * (perf['audience'] - 30))
 
     def calculate_performance_credits(perf, play):
         return Credits(max(perf['audience'] - 30, 0)). \
@@ -57,7 +36,7 @@ def statement(invoice, plays):
     for perf in invoice['performances']:
         performance = Performance(perf)
         play = plays[performance.play_id()]
-        this_amount = calculate_performance_amount(perf, play, performance)
+        this_amount = calculate_performance_amount(play, performance)
         performance_credits = calculate_performance_credits(perf, play)
 
         line = f' {play["name"]}: {format_as_dollars(this_amount.current() / 100)} ({performance.audience()} seats)\n'
