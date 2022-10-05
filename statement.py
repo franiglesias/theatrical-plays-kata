3b00044955279b66043378185fa63ac0534d79a3
Fran Iglesias <franiglesias@mac.com>
2022-10-05 19:40:57 +0200

    Remove abbreviation

--- statement.py
+++ statement.py
@@ -4,13 +4,13 @@ from domain.invoice import Invoice
 from domain.printer import Printer
 
 
-def statement(invoice, plays):
+def statement(invoice_data, plays):
     printer = Printer()
     invoice_amount = Amount(0)
     volume_credits = Credits(0)
-    inv = Invoice(invoice, plays)
+    invoice = Invoice(invoice_data, plays)
 
-    printer.print(f'Statement for {inv.customer()}\n')
+    printer.print(f'Statement for {invoice.customer()}\n')
 
     def format_as_dollars(amount):
         return f"${amount:0,.2f}"
@@ -18,7 +18,7 @@ def statement(invoice, plays):
     def formatted_line(title, audience, amount):
         return f' {title}: {format_as_dollars(amount.current() / 100)} ({audience} seats)\n'
 
-    for performance in inv.performances():
+    for performance in invoice.performances():
         printer.print(formatted_line(performance.title(), performance.audience(), performance.amount()))
         invoice_amount = invoice_amount.add(performance.amount())
         volume_credits = volume_credits.add(performance.credits())
