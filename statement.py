8dd101e27d4dfe431bd5860529245b0096ccfdd5
Fran Iglesias <franiglesias@mac.com>
2022-10-07 09:35:40 +0200

    Extract FormattedAmount decorator object

--- statement.py
+++ statement.py
@@ -2,21 +2,35 @@ from domain.invoice import Invoice
 from domain.printer import Printer
 
 
+class Line:
+    def __init__(self, title, audience, amount):
+        self.title = title
+        self.audience = audience
+        self.amount = amount
+
+    def formatted(self):
+        return f' {self.title}: {FormattedAmount(self.amount).dollars()} ({self.audience} seats)\n'
+
+
+class FormattedAmount:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def dollars(self):
+        return f"${self.amount.current() / 100:0,.2f}"
+
+
 def statement(invoice_data, plays):
-    def formatted_line(title, audience, amount):
-        return f' {title}: {format_as_dollars(amount.current() / 100)} ({audience} seats)\n'
-
-    def format_as_dollars(amount):
-        return f"${amount:0,.2f}"
-
-    printer = Printer()
-
     invoice = Invoice(invoice_data, plays)
 
+    printer = Printer()
     printer.print(f'Statement for {invoice.customer()}\n')
+
     for performance in invoice.performances():
-        printer.print(formatted_line(performance.title(), performance.audience(), performance.amount()))
-    printer.print(f'Amount owed is {format_as_dollars(invoice.amount().current() // 100)}\n')
+        line = Line(performance.title(), performance.audience(), performance.amount())
+        printer.print(line.formatted())
+
+    printer.print(f'Amount owed is {FormattedAmount(invoice.amount()).dollars()}\n')
     printer.print(f'You earned {invoice.credits().current()} credits\n')
 
     return printer.output()
